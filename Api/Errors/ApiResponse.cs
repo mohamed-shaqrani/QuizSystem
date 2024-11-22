211c08eeed8c2cdc9c4c5bbeb65d646f322d1cf5
Mohamed Shokrani <104221047+mohamed-shokrani@users.noreply.github.com>
2024-11-22 14:41:14 +0200

    Add ExceptionMiddleware

---
+++ Api/Errors/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace Api.Errors;
+
+public class ApiResponse
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+
+
+    public ApiResponse(int statusCode, string message = null)
+    {
+        StatusCode = statusCode;
+        Message = message ?? GetDefaulMessageForStatusCode(statusCode);
+
+    }
+    private string GetDefaulMessageForStatusCode(int statusCode)
+    {
+        return statusCode switch
+        {
+            400 => "A bad request, you have made",
+            401 => "Authorized, you are not",
+            404 => "Resource Found, it was not",
+            500 => "Server Error, the errors",
+            _ => ""
+        };
+    }
+}
---
+++ Api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Api.Errors;
+using System.Net;
+using System.Text.Json;
+
+namespace Api.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment environment)
+    {
+        _next = next;
+        _logger = logger;
+        _env = environment;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = _env.IsDevelopment()
+            ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+            : new ApiException((int)HttpStatusCode.InternalServerError);
+            var options =new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+            throw;
+        }
+    }
+}
